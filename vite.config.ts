8bb017957f5901278b6fc9ed0e34a2318e726da5
israeljuri <israeljuri0@gmail.com>
2026-03-19 20:13:23 +0100

    First commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { reactRouter } from '@react-router/dev/vite';
+import { cloudflare } from '@cloudflare/vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './app'),
+    },
+  }
+});
