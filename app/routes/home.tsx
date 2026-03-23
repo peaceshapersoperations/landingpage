3e68ffb896a0dc2c718345c9739b6a64826b26eb
israeljuri <israeljuri0@gmail.com>
2026-03-23 04:23:25 +0100

    Update

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,14 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="p-10 space-y-3">
+      <h1 className="text-7xl">Peace Shapers Africa</h1>
+      <p>
+        Peace Shapers Africa is building Nigeria’s first institutional safety
+        enterprise — working with schools and organisations to design, install,
+        and strengthen the safety systems that protect the people inside them.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
